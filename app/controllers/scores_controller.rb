778333e62df026ff728daad161aba13f8b8e4fa7
Andy <andrew.sunglae.kim@gmail.com>
2016-11-09 14:05:38 -0500

    Add rack cors and pry

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,3 +1,15 @@
 class ScoresController < ApplicationController
+  def index
+		@scores = Score.all
+    render json: @scores
+  end
 
+  def high_score
+    @scores = Score.where(maxNum: params[:score]).order(score: :desc, time: :asc)
+    render json: @scores
+  end
+
+  def score
+    binding.pry
+  end
 end
