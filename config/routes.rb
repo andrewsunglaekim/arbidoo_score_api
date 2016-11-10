29dfe0c55393d4281f35c8e6e4aaf6d0165f6271
Andy <andrew.sunglae.kim@gmail.com>
2016-11-09 21:05:12 -0500

    adds n to application ....

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.applicatio.routes.draw do
+Rails.application.routes.draw do
 	get 'highscores', to: "scores#high_score"
 	post 'highscore', to: "scores#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
