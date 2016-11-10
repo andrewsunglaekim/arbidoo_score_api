33d369a9454bbd376fa72c8e18cb9bbf835acdd4
Andy <andrew.sunglae.kim@gmail.com>
2016-11-09 21:00:19 -0500

    backend ... works

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,22 +1,35 @@
 class ScoresController < ApplicationController
+   before_action :get_high_scores, only: [:high_score, :create]
   def index
 		@scores = Score.all
     render json: @scores
   end
 
   def high_score
-    @scores = Score.where(maxNum: params[:score]).order(score: :desc, time: :asc).limit(5)
     render json: @scores
   end
 
   def create
     @score = Score.create!(score_params)
-    render json: @score
+		@data = {
+		  score: @score,
+			leaderBoard: @scores
+		}
+    render json: @data
   end
 
   private
   def score_params
    params.require(:score).permit(:username, :score, :maxNum, :operator, :time)
   end
+	
+	def get_high_scores
+	  if params[:score].class == "String"
+	    score = JSON.parse(params[:score])["maxNum"]
+		else
+			score = params[:score]["maxNum"]
+	  end
+    @scores = Score.where(maxNum: score).order(score: :desc, time: :asc).limit(5)
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-	get 'highscores/:score', to: "scores#high_score"
+Rails.applicatio.routes.draw do
+	get 'highscores', to: "scores#high_score"
 	post 'highscore', to: "scores#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
